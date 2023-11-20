1cd0f79228ab7bd8bb1d2fb72f8251b8a1651f3a
Jonchao <yijiefanren6@gmail.com>
2023-11-20 14:12:53 +0800

    Implement progressive rendering in homepage infinite scroll

--- components/HomepageInfiniteScroll.jsx
+++ components/HomepageInfiniteScroll.jsx
@@ -21,45 +21,42 @@ export default function HomepageInfiniteScroll() {
     console.log(dataToFetch);
     if (loading || !hasMoreData) return;
     setLoading(true);
-    const slidersData = await Promise.all(
-      categories.slice(dataToFetch[0], dataToFetch[1]).map(async (category) => {
-        let url;
-        if (genres.includes(category)) {
-          // 如果是流派类别，则构建流派 URL
-          url = `${process.env.API_URL}/anime/genres/${category}`;
-        } else if (directors.includes(category)) {
-          // 如果是流派类别，则构建流派 URL
-          url = `${process.env.API_URL}/anime/directors/${category}`;
-        } else {
-          // 否则，使用原来的 URL
-          url = `${process.env.API_URL}/anime/${category}`;
-        }
-
-        try {
-          const response = await axios.get(url, {
-            headers: {
-              "X-API-Key": process.env.API_KEY,
-            },
-          });
-          return { title: category, data: response.data };
-        } catch (error) {
-          console.error(`Error fetching ${category}`, error);
-          return { title: category, data: [] };
-        }
-      })
-    );
-    setItems((prevData) => [...prevData, ...slidersData]);
-    setDataToFetch((prevDataToFetch) => {
-      const nextStart = prevDataToFetch[1];
-      const nextEnd = nextStart + 6;
-
-      if (nextStart >= categories.length) {
-        setHasMoreData(false);
-        return prevDataToFetch;
+    for (
+      let i = dataToFetch[0];
+      i < Math.min(dataToFetch[1], categories.length);
+      i++
+    ) {
+      const category = categories[i];
+      let url;
+      if (genres.includes(category)) {
+        // 如果是流派类别，则构建流派 URL
+        url = `${process.env.API_URL}/anime/genres/${category}`;
+      } else if (directors.includes(category)) {
+        // 如果是流派类别，则构建流派 URL
+        url = `${process.env.API_URL}/anime/directors/${category}`;
+      } else {
+        // 否则，使用原来的 URL
+        url = `${process.env.API_URL}/anime/${category}`;
       }
 
-      return [nextStart, Math.min(nextEnd, categories.length)];
-    });
+      try {
+        const response = await axios.get(url, {
+          headers: { "X-API-Key": process.env.API_KEY },
+        });
+        setItems((prevData) => [
+          ...prevData,
+          { title: category, data: response.data },
+        ]);
+      } catch (error) {
+        console.error(`Error fetching ${category}`, error);
+        setItems((prevData) => [...prevData, { title: category, data: [] }]);
+      }
+    }
+
+    const nextStart = dataToFetch[1];
+    const nextEnd = nextStart + 6;
+    setDataToFetch([nextStart, Math.min(nextEnd, categories.length)]);
+    setHasMoreData(nextStart < categories.length);
     setLoading(false);
   };
 
