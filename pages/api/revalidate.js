db1f0280746bb9ae34373e0592b0a82b6ea15095
Jonchao <yijiefanren6@gmail.com>
2024-04-04 18:35:40 +0800

    fixed revalidate function

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -4,9 +4,10 @@ export default async function handler(req, res) {
   }
 
   try {
-    await res.unstable_revalidate("/");
+    await res.revalidate("/");
     return res.json({ revalidated: true });
   } catch (err) {
+    console.error(err);
     return res.status(500).send("Error revalidating");
   }
 }
